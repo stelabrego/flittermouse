592643255929a228184bd11f4f580c1e69e1cead
Stel Abrego <stelabrego@icloud.com>
2019-08-13 15:34:18 -0400

    remove unecessary line

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-path.normalize("../build/views")
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
