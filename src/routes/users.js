c1ff9eac553402006814c49f107c9e27c117e791
Stel Abrego <stelabrego@icloud.com>
2019-08-14 23:10:49 -0400

    get users/add route working

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let crypto = require('crypto');
+let getDatabase = require('../lib/getDatabase')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Add user
+router.post('/add', function (req, res, next) {
+  let userKey = crypto.randomBytes(6).toString('hex');
+  let columns = ['username', 'password', 'email', 'display_name', 'phone_number', 'address', 'avatar_url', 'bio']
+  let values = columns.reduce((prev, curr) => {
+    prev['$' + curr] = req.body[curr] || null;
+    return prev;
+  }, { $key: userKey });
+  let statement = "INSERT INTO user (username, password, email, key, display_name, phone_number, address, avatar_url, bio) VALUES ($username, $password, $email, $key, $display_name, $phone_number, $address, $avatar_url, $bio)";
+  let db = getDatabase((err) => {
+    res.send({ success: false, message: err.message });
+  })
+  db.run(statement, values, (err) => {
+    if (err) res.send({ success: false, message: err.message });
+    else res.send({ success: true });
+  });
+  db.close();
 });
 
 module.exports = router;
