86c855fd865fd1488bdbb69133d4f7812e4311d3
Stel Abrego <stelabrego@icloud.com>
2019-08-19 12:59:45 -0400

    add event testing

--- test/api.test.js
+++ test/api.test.js
@@ -7,13 +7,14 @@ const goodUsers = [
   { username: 'lovenoone', password: 'hibob', email: 'emogoth@gmail.com' },
   { username: 'anlasifs', password: 'password', email: 'haonicnen@gmail.com' }
 ]
-
 const badUsers = [
   { username: 'hithere', email: 'generic@gmail.com' }
 ]
-
 const goodEvents = [
   { name: 'stel bday party 2' }
+]
+const badEvents = [
+  { name: 'christmas partayy', christmas: true }
 ]
 
 describe('database api', () => {
@@ -36,10 +37,10 @@ describe('database api', () => {
         .expect(200)
     })
     it('should accept new users with correct schema', () => {
-      goodUsers.forEach((val) => {
+      goodUsers.forEach((user) => {
         request(app)
           .post('/users/add')
-          .send({ username: 'alkjslsfi', password: 'skjdhfkjd', email: 'lsfdjsskhsh' })
+          .send(user)
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect((res) => {
@@ -49,10 +50,56 @@ describe('database api', () => {
       })
     })
     it('should reject new users with incorrect schema', () => {
-      badUsers.forEach((val) => {
+      badUsers.forEach((user) => {
         request(app)
           .post('/users/add')
-          .send({ username: 'alkjslsfi', password: 'skjdhfkjd', email: 'lsfdjsskhsh' })
+          .send(user)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            assert(res.body.success === true, JSON.stringify(res.body))
+          })
+          .expect(200)
+      })
+    })
+  })
+
+  describe('/events/add', () => {
+    beforeEach((done) => {
+      exec('make db', (err, stdout, stderr) => {
+        if (err) throw done(err)
+        done()
+      })
+    })
+    it('should not accept empty request', () => {
+      request(app)
+        .post('/events/add')
+        .send({})
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect((res) => {
+          assert(res.body.success === false, JSON.stringify(res.body))
+        })
+        .expect(200)
+    })
+    it('should accept new events with correct schema', () => {
+      goodEvents.forEach((event) => {
+        request(app)
+          .post('/events/add')
+          .send(event)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            assert(res.body.success === true, JSON.stringify(res.body))
+          })
+          .expect(200)
+      })
+    })
+    it('should reject new events with incorrect schema', () => {
+      badEvents.forEach((val) => {
+        request(app)
+          .post('/events/add')
+          .send(val)
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect((res) => {
