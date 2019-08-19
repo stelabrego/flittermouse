a6897a32b07b1e842ae87990aa67d7012fb0600b
Stel Abrego <stelabrego@icloud.com>
2019-08-19 16:00:39 -0400

    fix databse name

--- src/lib/getDatabase.js
+++ src/lib/getDatabase.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose()
 const path = require('path')
 
 const getDatabase = (callback) => {
-  const dbpath = path.resolve(__dirname, '../eventz.db')
+  const dbpath = path.resolve(__dirname, '../../build/eventz.db')
   return new sqlite3.Database(dbpath, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       callback(err)
