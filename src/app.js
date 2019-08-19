03b24cc99ba3f5f00bfc2f6b5028395c3bbee382
Stel Abrego <stelabrego@icloud.com>
2019-08-19 16:01:52 -0400

    add morgan logger

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 // var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
+var logger = require('morgan')
+const fs = require('fs')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -13,7 +14,8 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// app.use(logger('dev'));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../build/access.log'), { flags: 'a' })
+app.use(logger('combined', { stream: accessLogStream }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 // app.use(cookieParser());
