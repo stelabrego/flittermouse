26d37c4d21f57c49643ff700f86c5f9dbb0c4749
Stel Abrego <stelabrego@icloud.com>
2019-09-06 16:29:53 -0400

    add more test users

--- src/db/createMockData.js
+++ src/db/createMockData.js
@@ -4,10 +4,17 @@ module.exports = async () => {
   try {
     // Can't manually insert rows with id column without manually updating the user_id_seq value or else the serial function fucks up
     // See: https://stackoverflow.com/questions/9108833/postgres-autoincrement-not-updated-on-explicit-id-inserts
+    // don't forget to escaple apostraphes with by adding two ''
+    console.log('Attempting test data entry')
     await db.query(`INSERT INTO users (username, email, password_hash, invite_key, display_name, bio) VALUES
-        ('stel', 'stel@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key1', 'Stel Abrego', 'I''m here and I''m queer'),
-        ('alice', 'alice@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key2', 'sexyalice666', 'I like movies'),
-        ('ash', 'ash@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key3', 'ashthisblunt', 'smoke')
+        ('stel', 'stel@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key1', 'stel', 'I''m here and I''m queer'),
+        ('sexyalice666', 'alice@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key2', 'alice', 'I like movies'),
+        ('ashthisblunt', 'ash@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key3', 'ash', 'smoke'),
+        ('carta123', 'carta@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key4', 'carta', 'i make comics'),
+        ('pikachu_addiction', 'ej@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key5', 'EJ', 'gym life'),
+        ('comic_luver', 'casey@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key6', 'casey', 'i''m probably at michigan adventures'),
+        ('mutual_aid', 'vidhya@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key7', 'vidhya', 'liberation is cool i guess'),
+        ('doubletrans', 'renee@gmail.com', crypt('123456', gen_salt('md5')), 'invite_key8', 'renée', 'it''s time for tea')
       `)
     await db.query(`INSERT INTO events (user_id, url_key, name, location, lat, lon, description, date_start, date_end) VALUES
         (1, 'test', 'stels big party', '306 N Adams St Ypsilanti', 42.245083, -83.615924, 'We''re really just gonna smoke weed and compliment eachother', '2019-10-31 18:00', '2019-10-31 23:00'),
