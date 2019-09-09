5b6620c428005828b15643cd17ff8e12da5d3b92
Stel Abrego <stelabrego@icloud.com>
2019-09-09 17:13:00 -0400

    hide secrets from repo for security

--- src/db/index.js
+++ src/db/index.js
@@ -2,26 +2,26 @@ const { Pool } = require('pg')
 
 const {
   NODE_ENV = 'development',
-  PGHOST = 'localhost',
-  PGUSER = 'postgres',
-  PGDATABASE = 'flittermouse',
-  PGPASSWORD = 'thecatsatonthestoolandplayedthepiano',
-  PGPORT = 5432
+  POSTGRES_HOST = 'localhost',
+  POSTGRES_USER = 'postgres',
+  POSTGRES_DB = 'flittermouse',
+  POSTGRES_PASSWORD = 'thecatsatonthestoolandplayedthepiano',
+  POSTGRES_PORT = 5432
 } = process.env
 
 let pool
 if (NODE_ENV === 'production') {
-  const connectionString = `postgresql://${PGUSER}:${PGPASSWORD}@db/${PGDATABASE}`
+  const connectionString = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db/${POSTGRES_DB}`
   pool = new Pool({
     connectionString
   })
 } else {
   pool = new Pool({
-    host: PGHOST,
-    user: PGUSER,
-    database: PGDATABASE,
-    password: PGPASSWORD,
-    port: PGPORT
+    host: POSTGRES_HOST,
+    user: POSTGRES_USER,
+    database: POSTGRES_DB,
+    password: POSTGRES_PASSWORD,
+    port: POSTGRES_PORT
   })
 }
 module.exports = {
